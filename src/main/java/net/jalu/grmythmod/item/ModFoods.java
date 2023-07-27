208c6a85681fa4932b5c59041a7b96043f755353
Jackson <jl14u21@soton.ac.uk>
2023-07-27 12:45:35 +0300

    Food item works

--- src/main/java/net/jalu/grmythmod/item/ModFoods.java
+++ src/main/java/net/jalu/grmythmod/item/ModFoods.java
@@ -1,6 +1,5 @@
 package net.jalu.grmythmod.item;
 
-import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
@@ -9,6 +8,6 @@ public class ModFoods {
 
     public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
         .saturationMod(0.2f)
-        .effect(() -> new MobEffectInstance(MobEffects.GLOWING, 2000), 0.1f).build();
+        .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.5f).build();
 
 }
