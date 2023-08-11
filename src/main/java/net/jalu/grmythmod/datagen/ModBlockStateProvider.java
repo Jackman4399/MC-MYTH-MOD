c119eabc72f0b10e0819cc6cb264869c5bd0ec59
Jackson <jl14u21@soton.ac.uk>
2023-08-11 16:49:32 +0300

    Progess till 8:08

--- src/main/java/net/jalu/grmythmod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/jalu/grmythmod/datagen/ModBlockStateProvider.java
@@ -1,4 +1,31 @@
 package net.jalu.grmythmod.datagen;
 
-public class ModBlockStateProvider {
+import net.jalu.grmythmod.MythMod;
+import net.jalu.grmythmod.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, MythMod.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        blockWithItem(ModBlocks.RUBY_ORE);
+        blockWithItem(ModBlocks.DEEPSLATE_RUBY_ORE);
+        blockWithItem(ModBlocks.END_STONE_RUBY_ORE);
+        blockWithItem(ModBlocks.NETHER_RUBY_ORE);
+        blockWithItem(ModBlocks.RUBY_BLOCK);
+
+        blockWithItem(ModBlocks.SOUND_BLOCK);
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
 }
--- src/main/java/net/jalu/grmythmod/datagen/ModItemModelProvider.java
+++ src/main/java/net/jalu/grmythmod/datagen/ModItemModelProvider.java
@@ -1,4 +1,37 @@
 package net.jalu.grmythmod.datagen;
 
-public class ModItemModelProvider {
+import net.jalu.grmythmod.MythMod;
+import net.jalu.grmythmod.item.ModItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, MythMod.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+
+        simpleItem(ModItems.RUBY);
+        simpleItem(ModItems.RAW_RUBY);
+
+        simpleItem(ModItems.NECTAR);
+        simpleItem(ModItems.PINE_CONE);
+
+        simpleItem(ModItems.METAL_DETECTOR);
+        simpleItem(ModItems.STRAWBERRY);
+
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+            new ResourceLocation("item/generated")).texture("layer0",
+            new ResourceLocation(MythMod.MOD_ID, "item/" + item.getId().getPath()));
+    }
 }
